2cc96fe26939c9aba1bfd6fdf7110d77527ed975
toddRox <toddhensley@protonmail.com>
2026-04-03 11:07:01 -0400

    Added logic to 4 functions in Tester.cs.

--- CodeTest/Tester.cs
+++ CodeTest/Tester.cs
@@ -15,17 +15,23 @@ namespace CodeTest
         /// <returns>bool</returns>
         public static bool IsPalindrome(string input)
         {
-            return false;
+            return input == null ? false : string.Join("", input.Reverse()).Equals(input);
         }
 
         /// <summary>
         /// Function that takes a string and returns the count of each character in the string
         /// </summary>
-        /// <param name="inpput">input string</param>
+        /// <param name="input">input string</param>
         /// <returns>Dictionary with each character from the string as the key and the count of each charter as the value</returns>
-        public static Dictionary<char, int> CharacterCount(string inpput)
+        public static Dictionary<char, int> CharacterCount(string input)
         {
             Dictionary<char, int> output = new Dictionary<char, int>();
+            if(input == null) return output;
+
+            foreach (char c in input)
+            {
+                output[c] =  output.GetValueOrDefault(c, 0) + 1;
+            }
 
             return output;
         }
@@ -39,9 +45,12 @@ namespace CodeTest
         /// <returns>an array of integers</returns>
         public static int[] GetMatches(int[] input1, int[] input2)
         {
-            int[] output =  new int[0];            
+            if(input1 == null || input2 == null) throw new ArgumentException("Parameters cannot be null.");
 
-            return output;
+            var set1 = new HashSet<int>(input1);
+            var set2 = new HashSet<int>(input2);
+
+            return set1.Intersect(set2).ToArray();
         }
 
 
@@ -53,9 +62,15 @@ namespace CodeTest
         /// <returns>an array of integers</returns>
         public static int[] GetDiff(int[] input1, int[] input2)
         {
-            int[] output = new int[0];
+            if(input1 == null || input2 == null) throw new ArgumentException("Parameters cannot be null.");
 
-            return output;
+            var set1 = new HashSet<int>(input1);
+            var set2 = new HashSet<int>(input2);
+
+            var diff1 = set1.Except(set2).ToArray();
+            var diff2 = set2.Except(set1).ToArray();
+
+            return diff1.Union(diff2).ToArray();
         }
     }
 }
